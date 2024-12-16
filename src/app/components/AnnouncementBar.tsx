fb45af998cdc86801cdbc24f92b866ba6ebed7fb
Awointa <akosawointa@gmail.com>
2024-12-16 10:46:57 +0100

    feat: add anouncement bar

--- src/app/components/AnnouncementBar.tsx
+++ src/app/components/AnnouncementBar.tsx
@@ -2,15 +2,17 @@ import LanguageOptions from "./LanguageOptions";
 
 export default function announcementBar() {
 	return (
-		<div className="bg-black flex justify justify-center items-center gap-[20%]">
-			<div className=" text-white py-3 flex items-center justify-center gap-2">
-				<p>
-					Summer Sales For All Swim Suits And Free Express Delivery - OFF 50%!{" "}
-				</p>
-				<a className="underline">ShopNow</a>
-			</div>
+		<div className="m-auto">
+			<div className="bg-black flex justify justify-center items-center gap-[10%]">
+				<div className=" text-white py-3 flex items-center justify-center gap-2">
+					<p>
+						Summer Sales For All Swim Suits And Free Express Delivery - OFF 50%!{" "}
+					</p>
+					<a className="underline">ShopNow</a>
+				</div>
 
-			<LanguageOptions />
+				<LanguageOptions />
+			</div>
 		</div>
 	);
 }
