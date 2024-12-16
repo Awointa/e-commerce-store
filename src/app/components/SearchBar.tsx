e35c8140d1b059afa3734ef7347af1e9e73ea1a2
Awointa <akosawointa@gmail.com>
2024-12-16 10:49:41 +0100

    feat: add Serachbar

---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+function SearchBar() {
+	return (
+		<div>
+			<input
+				type="text"
+				placeholder="What are you looking for?"
+				className="pl-5 py-[7px] pr-3 text-sm w-60"
+			/>
+		</div>
+	);
+}
+
+export default SearchBar;
