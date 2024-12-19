3ea3cc56abb98cb2b9b8c6c72e87dae66d27cb6b
Awointa <akosawointa@gmail.com>
2024-12-19 23:00:39 +0100

    feat: import and render NavBar and and Footer in the app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
 import "./globals.css";
 import AnnouncementBar from "./components/AnnouncementBar";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-export default function RootLayout() {
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
 	return (
 		<html lang="en">
-			<body>
+			<body className="bg-white-smoke grid grid-rows-[auto_1fr_auto] h-screen">
 				<header>
 					<AnnouncementBar />
+					<NavBar />
 				</header>
+				<main>{children}</main>
+				<Footer />
 			</body>
 		</html>
 	);
