6e15e7f73e12f0805ac562fb91f9a845fdfead70
Awointa <akosawointa@gmail.com>
2025-01-09 12:06:34 +0100

    feat: include StarRating feature

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 import Image from "next/image";
@@ -15,14 +17,22 @@ const ProductCard = () => {
 				</div>
 			</div>
 
-			<div className="flex flex-col">
+			<div className="flex flex-col gap-2 pt-4">
 				<p className="font-bold ">HAVIT HV-G92 Gamepad</p>
 				<div className="flex gap-3">
 					<span className="text-gino-red">&#8358; 5000</span>
 					<span className="text-ash-smoke line-through">&#8358; 10000</span>
 				</div>
+				<StarRating
+					maxRating={5}
+					color="#FFAD33"
+					size={20}
+					className="star-rating"
+					messages={["1", "2", "4", "4", "5"]}
+					defaultRating={0}
+					onSetRating={(rating) => console.log(`Rating set to ${rating}`)}
+				/>
 			</div>
-			<StarRating />
 		</div>
 	);
 };
