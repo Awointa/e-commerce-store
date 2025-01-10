03837fd55dd012deb7754f393732e1455091d96f
Awointa <akosawointa@gmail.com>
2025-01-10 10:50:02 +0100

    create primary button

---
+++ src/app/components/PrimaryButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrimaryButton = () => {
+	return (
+		<button className="bg-gino-red text-white-smoke text-[16px] px-12 py-4 rounded-md">
+			view all products
+		</button>
+	);
+};
+
+export default PrimaryButton;
