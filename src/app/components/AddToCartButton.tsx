671c29450f8dbb2f296c0d455a372a3ebea3afcd
Awointa <akosawointa@gmail.com>
2025-01-14 14:54:30 +0100

    create AddToCartButton

---
+++ src/app/components/AddToCartButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddToCartButton = () => {
+	return (
+		<button className="bg-black w-full h-[41px] text-white-smoke cursor-pointer rounded-b-md  ">
+			Add To Cart
+		</button>
+	);
+};
+
+export default AddToCartButton;
