67fdab64fdb3f194dc062c28b02d24d490d51ea8
Awointa <akosawointa@gmail.com>
2025-01-14 15:36:00 +0100

    feat: create pre-styled CategoriesSection

--- src/app/components/CategoriesSection.tsx
+++ src/app/components/CategoriesSection.tsx
@@ -1,5 +1,23 @@
+import ArrowButton from "./ArrowButton";
+import CategoriesSlider from "./CategoriesSlider";
+import SectionLabel from "./SectionLabel";
+
 const CategoriesSection = () => {
-	return <section></section>;
+	return (
+		<section className="pt-20 border-b-0.5 border-gray-300">
+			<SectionLabel text="Categories" />
+			<div className="flex items-center justify-between pt-3">
+				<h4 className="text-4xl font-bold">Browse by Category</h4>
+				<div className="flex items-center gap-2">
+					<ArrowButton type="left" />
+					<ArrowButton type="right" />
+				</div>
+			</div>
+			<div className="py-[60px]">
+				<CategoriesSlider />
+			</div>
+		</section>
+	);
 };
 
 export default CategoriesSection;
