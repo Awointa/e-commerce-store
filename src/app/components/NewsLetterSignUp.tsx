b3479fa9ea9382cb81476c81664b27f6a1d176cb
Awointa <akosawointa@gmail.com>
2025-01-15 10:36:23 +0100

    feat: styled the NewsLetter input box

--- src/app/components/NewsLetterSignUp.tsx
+++ src/app/components/NewsLetterSignUp.tsx
@@ -1,7 +1,11 @@
 function NewsLetterSignUp() {
 	return (
 		<div>
-			<input type="email" />
+			<input
+				type="email"
+				className="w-full py-3 px-4 bg-inherit border-0.5 border-white-smoke rounded-md"
+				placeholder="Enter your email"
+			/>
 		</div>
 	);
 }
