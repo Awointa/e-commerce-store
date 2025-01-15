d5b27670ab6c0f076a0e77091cf3bda90f0b52a1
Awointa <akosawointa@gmail.com>
2025-01-15 10:46:55 +0100

    feat: added BestSellingSection FeatureBanner ExploreProductSection FeaturedSection BenefitsSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,14 @@ import ImageSlider from "./components/ImageSlider";
 import slide1 from "./SliderImages/slide1.png";
 import slide2 from "./SliderImages/slide1.png";
 import slide3 from "./SliderImages/slide1.png";
+
 import FlashSalesSection from "./components/FlashSalesSection";
 import CategoriesSection from "./components/CategoriesSection";
+import BestSellingSection from "./components/BestSellingSection";
+import FeatureBanner from "./components/FeatureBanner";
+import ExploreProductsSection from "./components/ExploreProductsSection";
+import FeaturedSection from "./components/FeaturedSection";
+import BenefitsSection from "./components/BenefitsSection";
 
 const images = [slide1, slide2, slide3];
 
@@ -21,6 +27,14 @@ function page() {
 			</div>
 			<FlashSalesSection />
 			<CategoriesSection />
+			<BestSellingSection />
+			<div className="pt-40">
+				{" "}
+				<FeatureBanner />
+			</div>
+			<ExploreProductsSection />
+			<FeaturedSection />
+			<BenefitsSection />
 		</div>
 	);
 }
