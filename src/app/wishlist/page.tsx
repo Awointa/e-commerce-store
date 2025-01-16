8d94ab5faae16b698bdcfcecc1e2da666fa7c72a
Awointa <akosawointa@gmail.com>
2025-01-16 15:56:16 +0100

    feat: create the for you section

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,15 +1,27 @@
 import PrimaryButton from "../components/PrimaryButton";
+import SectionLabel from "../components/SectionLabel";
 import WishListProductCard from "../components/WishListProductCard";
 
 const page = () => {
 	return (
-		<div className="px-[135px] pt-20">
-			<div className="flex items-center justify-between">
-				<h2 className="text-xl">Wishlist (4)</h2>
-				<PrimaryButton text="Move All To Bag" type="monochrome" />
+		<div className="px-[135px] mt-20 mb-60">
+			<div>
+				<div className="flex items-center justify-between">
+					<h2 className="text-xl">Wishlist (4)</h2>
+					<PrimaryButton text="Move All To Bag" type="monochrome" />
+				</div>
+				<div className="grid grid-cols-4 gap-6 mt-16 ">
+					<WishListProductCard type="wishlist" />
+				</div>
 			</div>
-			<div className="grid grid-cols-4 gap-6 mt-16 mb-20">
-				<WishListProductCard />
+			<div className="mt-20">
+				<div className="flex items-center justify-between">
+					<SectionLabel text="Just For You" />
+					<PrimaryButton text="See All" type="monochrome" />
+				</div>
+				<div className="grid grid-cols-4 gap-6 mt-16 ">
+					<WishListProductCard type="for-you" />
+				</div>
 			</div>
 		</div>
 	);
