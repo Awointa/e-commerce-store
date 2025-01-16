391eeb924a543c9849e0b75602da6eb1c5071ecb
Awointa <akosawointa@gmail.com>
2025-01-16 15:14:55 +0100

    feat: create wishlist section

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,5 +1,18 @@
+import PrimaryButton from "../components/PrimaryButton";
+import WishListProductCard from "../components/WishListProductCard";
+
 const page = () => {
-	return <div></div>;
+	return (
+		<div className="px-[135px] pt-20">
+			<div className="flex items-center justify-between">
+				<h2 className="text-xl">Wishlist (4)</h2>
+				<PrimaryButton text="Move All To Bag" type="monochrome" />
+			</div>
+			<div className="grid grid-cols-4 gap-6 mt-16 mb-20">
+				<WishListProductCard />
+			</div>
+		</div>
+	);
 };
 
 export default page;
