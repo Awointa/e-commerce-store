1f797ce0f427245a4eeceb4a69dc28dc911cd8c2
Awointa <akosawointa@gmail.com>
2025-01-18 09:05:12 +0100

    feat:create cartItem component

---
+++ src/app/components/CartItem.tsx
@@ -0,0 +1,12 @@
+const CartItem = () => {
+	return (
+		<div className="flex">
+			<div>LCD Monitor</div>
+			<div>200000</div>
+			<div>[]</div>
+			<div>200000</div>
+		</div>
+	);
+};
+
+export default CartItem;
