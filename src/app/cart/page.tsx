8bfddeae3b7a2382cfda3b9676f0c3ceddfaebf7
Awointa <akosawointa@gmail.com>
2025-01-23 12:33:06 +0100

    feat: add monitor image

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,23 @@
-const page = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import Link from "next/link";
 
-export default page
+const page = () => {
+	return (
+		<div className="mx-[135px] mt-20 mb-60">
+			<nav className="flex items-center space-x-2 text-sm">
+				<Link href="/">Home</Link>
+				<span>/</span>
+				<Link href="/">Cart</Link>
+			</nav>
+			<div className="mt-20">
+				<div className="flex justify-between py-6 px-10 bg-white shadow-md rounded-sm">
+					<div>Product</div>
+					<div>Price</div>
+					<div>Quantity</div>
+					<div>Subtotal</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default page;
