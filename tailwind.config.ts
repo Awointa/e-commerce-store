8020b8f8891aa659ecd44b994c57a04493c520b1
Awointa <akosawointa@gmail.com>
2025-02-18 20:00:39 +0100

    refactor: remove unused color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import forms from "@tailwindcss/forms";
 
 export default {
 	content: [
@@ -11,7 +12,6 @@ export default {
 		extend: {
 			colors: {
 				"white-smoke": "#F5F5F5",
-				"white-smoke-2": "#F2F2F2",
 				"ash-smoke": "#7D8184",
 				"dark-ash-smoke": "#2F2E30",
 				"gino-red": "#DB4444",
@@ -19,6 +19,5 @@ export default {
 			},
 		},
 	},
-
-	plugins: [],
+	plugins: [forms],
 } satisfies Config;
