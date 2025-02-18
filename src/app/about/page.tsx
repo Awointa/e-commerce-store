57ddf2657ec9489125ab7e75201403c8a8c986cf
Awointa <akosawointa@gmail.com>
2025-02-18 19:48:41 +0100

    feat: create the about page component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+const page = () => {
+	return (
+		<div className="mt-29 mx-[135px] mb-36">
+			<div className="flex text-sm">
+				<div>Home</div>
+				<span className="px-3 ">/</span>
+				<div>About</div>
+			</div>
+		</div>
+	);
+};
+
+export default page;
